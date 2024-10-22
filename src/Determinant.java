5bc3852c49e4b55df9494d867e1d1c0084435b7d
susTuna <hidasetsuna@gmail.com>
2024-10-22 16:27:03 +0700

    feat: applied CopyMatrix to Determinant to fix InverseOBE bug

--- src/Determinant.java
+++ src/Determinant.java
@@ -3,33 +3,34 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Determinant {
-    public static float det(Matrix m){
-        float err = -405;
-        float det = 1;
+    public static double det(Matrix m){
+        double err = -405;
+        double det = 1;
+        Matrix copy = m.copyMatrix(m);
         /* Ubah Ke Matrix Eselon */
-        int N=m.row;
+        int N=copy.row;
         for (int i = 0; i < N; i++) {
             // Step 1: Find pivot (leading non-zero element in current column)
-            if (m.elmt(i, i) == 0) {
+            if (copy.elmt(i, i) == 0) {
                 for (int j = i + 1; j < N; j++) {
-                    if (m.elmt(j, i) != 0) {
-                        m.swapRow(i, j);
+                    if (copy.elmt(j, i) != 0) {
+                        copy.swapRow(i, j);
                         break;
                     }
                 }
             }
             // Step 3: Eliminate all elements below the pivot in the current column
             for (int j = i + 1; j < N; j++) {
-                if(!m.isZeroRow(j)){
-                    float factor = -m.elmt(j, i)/m.elmt(i,i);
-                    m.addRow(j, i, factor);
+                if(!copy.isZeroRow(j)){
+                    double factor = -copy.elmt(j, i)/copy.elmt(i,i);
+                    copy.addRow(j, i, factor);
                 }
                 
             }
         }
 
         for(int i=0;i<N;i++){
-            det*=m.elmt(i,i);
+            det*=copy.elmt(i,i);
         }
         return det;
     }
@@ -37,7 +38,7 @@ public class Determinant {
     
 
     public static void main(String[] args) {
-        float ans;
+        double ans;
         Scanner in = new Scanner(System.in);
         System.out.print("Enter the number of rows: ");
         int rows = in.nextInt();
