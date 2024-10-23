2bdf8dcab30922b1c0df7577503a8a08419e2ffe
Samantha Laqueenna Ginting <167011131+sammmine@users.noreply.github.com>
2024-10-23 23:40:05 +0700

    fix: fix errors

--- src/RegLinier.java
+++ src/RegLinier.java
@@ -1,56 +1,110 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class RegLinier {
     public static double[] regLin(Matrix m, Scanner in) {
         double[] err = {-405};
         
-        // store  x values (other than first column)
+        // store x values (other than first column)
         Matrix x = new Matrix(m.row, m.col-1);
         for (int i=0; i<m.row; i++) {
             for (int j=1; j<m.col; j++) {
-                x.set(i, j, m.elmt(i, j));
+                x.set(i, j-1, m.elmt(i, j));
             }
         }
         
         // store y values (first column)
-        double[] y = new double[m.row];
+        Matrix y = new Matrix(m.row, 1);
         for (int i=0; i<m.row; i++) {
-            y[i] = m.elmt(i, 0);
+            y.set(i, 0, m.elmt(i, 0));
         }
         
         // hitung x^T * x
-        Matrix xt = Matrix.transposeMatrix(m);
-        Matrix xtx = Matrix.multMatrix(xt, m);
+        Matrix xt = Matrix.transposeMatrix(x);
+        Matrix xtx = Matrix.multMatrix(xt, x); // row = xt.row ; col = x.col
 
         // hitung x^T * y
-        Matrix yMatrix = new Matrix(m.row, 1);
-        for (int i=0; i<m.row; i++) {
-            yMatrix.set(i, 0, y[i]);
-        }
+        Matrix xty = Matrix.multMatrix(xt, y); // row = xt.row ; col = y.col
 
-        Matrix xty = Matrix.multMatrix(xt, yMatrix);
-
-        Matrix temp = new Matrix(m.row, m.col);
-
+        Matrix temp = new Matrix(xt.row, x.col+y.col);
         // gabungkan xtx dan xty
-        for (int i=0; i<m.row; i++) {
-            for (int j=0; j<m.col; j++) {
-                if (j < m.col-1) {
-                    temp.set(i, j, xtx.elmt(i, j));
-                } else {
-                    temp.set(i, j, xty.elmt(i, 0));
+        for (int i=0; i<temp.row; i++) {
+            for (int j=0; j<xtx.col; j++) {
+                temp.set(i, j, xtx.elmt(i, j));
                 }
+            for (int j=0; j<xty.col; i++) {
+                temp.set(i, j+xty.col, xty.elmt(i, 0));
+            }
+        }
+        
+        
+        Matrix solusiM = Gauss.mgauss(temp, in);
+
+        double[] solusi2 = new double[m.col-1];
+        boolean[] isFree = new boolean[m.col-1];
+        boolean unsolv=false;
+        boolean hasFreeVar = false;
+        List<Integer> freeVar = new ArrayList<>();
+        /* Lakukan Substitusi Balik */
+        for(int i=solusiM.row-1;i>=0;i--){
+            if(solusiM.isZeroRow(i)&&solusiM.elmt(i,solusiM.col-1)!=0){
+                return err;
+            }
+            else if(solusiM.isZeroRow(i)){
+                freeVar.add(i); //variabel bebas
+            }
+            double sum=0;
+            for(int j=i+1;j<solusiM.col-1;j++){
+                sum+=m.elmt(i,j)*solusi2[j];
+            }
+            solusi2[i]=(solusiM.elmt(i,solusiM.col-1)-sum)/solusiM.elmt(i,i);
+        }
+        /* Menentukan adanya variabel bebas */
+        for(int i=0;i<solusiM.row;i++){
+            boolean foundPivot = false;
+            for(int j=0;j<solusiM.col-1;j++){
+                if(solusiM.elmt(i,j)!=0){
+                    foundPivot=true;
+                    break;
+                }
+            }
+            if(!foundPivot){
+                hasFreeVar=true; //variabel bebas ditemukan
+                isFree[i]=true;
             }
         }
 
-        double[] solusi = Gauss.gauss(temp, in);
-
-        if (solusi == err) {
+        /* Keluaran */
+        if(unsolv){
             System.out.println("Tidak dapat mencari solusi regresi.");
             return err;
-        } 
-
-        return solusi;
+        }
+        else if(hasFreeVar){
+            System.out.println("Hasil perhitungan regresi menggunakan metode Gauss :");
+            for(int i=0;i<solusiM.col-1;i++){
+                if(isFree[i]){
+                    System.out.println("beta"+(i+1)+" : variabel bebas");
+                }
+                else{
+                    System.out.print("beta"+(i+1)+ " : ");
+                    for(int j=i+1;j<solusiM.col-1;j++){
+                        if(solusiM.elmt(i,j)!=0){
+                            System.out.print(solusiM.elmt(i,j)+" * variabel bebas ");
+                        }  
+                    }
+                    System.out.println();
+                }
+            }
+            return err;
+        }
+        else{
+            System.out.println("Hasil perhitungan regresi menggunakan metode Gauss :");
+            for(int i=0;i<solusi2.length;i++){
+                System.out.println("x"+(i+1)+" : "+solusi2[i]);
+            }
+            return solusi2;
+        }
     }
 
     public static void main(String[] args) {
@@ -67,7 +121,7 @@ public class RegLinier {
 
         System.out.println("Solusi regresi linier:");
         for (int i=0; i < ans.length; i++) {
-            System.out.print("beta" + i+1 + " = " + ans[i]);
+            System.out.print("beta" + (i+1) + " = " + ans[i]);
         }
     }
 }
