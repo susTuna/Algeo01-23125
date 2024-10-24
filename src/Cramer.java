86a786a0eb12500d0f77766c3bb369999782f192
Samantha Laqueenna Ginting <167011131+sammmine@users.noreply.github.com>
2024-10-24 12:43:12 +0700

    chore: clean up code and add input conditions

--- src/Cramer.java
+++ src/Cramer.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class Cramer {
     public static double[] cramer(Matrix m, Scanner in){
         double[] err = {-405};
-        boolean unsolv=false;
         double[] solusi = new double[m.row];
         Matrix temp = new Matrix(m.row, m.col-1);
         double dete = Determinant.det(m, in);
@@ -22,33 +21,56 @@ public class Cramer {
                 // Swap back the column
                 m.swapCol(i, m.col-1);
             }
-        } else {
-            unsolv = true;
-        }
 
-        if(unsolv){
+            System.out.println("Solusi SPL:");
+            for (int i = 0; i < solusi.length; i++) {
+                System.out.println("x" + (i+1) + " : " + solusi[i]);
+            }
+
+            return solusi;
+        } else {
             System.out.println("Tidak dapat mencari solusi kaidah cramer karena determinan matriks = 0.");
             return err;
         }
-
-        return solusi;
     }
 
-    public static void main(String[] args) {
-        double[] ans;
+    public static void call() {
+        double[] solusi;
         Scanner in = new Scanner(System.in);
 
-        Matrix mat;
-        mat=ReadWrite.txtRead(in);
-        
-        System.out.println("Matriks sebelum operasi Crammer:");
-        mat.printMatrix();
+        int choice;
+        choice = ReadWrite.fileOrKeys(in);
 
-        ans = cramer(mat, in);
+        if (choice == 1) {
+            System.out.print("Masukkan jumlah baris: ");
+            int rows = in.nextInt();
+            System.out.print("Masukkan jumlah kolom: ");
+            int cols = in.nextInt();
+            
+            Matrix m1 = new Matrix(rows, cols);
+            System.out.println("Masukkan matriks:");
+            m1.readMatrix(in);
+            
+            System.out.println("Matriks sebelum operasi Cramer:");
+            m1.printMatrix();
+            
+            // Print solusi
+            solusi=cramer(m1, in);
 
-        System.out.println("Solusi SPL:");
-        for (int i = 0; i < ans.length; i++) {
-            System.out.println("x" + (i+1) + " : " + ans[i]);
+            // Write to file
+            ReadWrite.arrToFile(solusi, in);
+        } else if (choice == 2) {
+            Matrix m2;
+            m2=ReadWrite.txtRead(in);
+
+            System.out.println("Matriks sebelum operasi Cramer:");
+            m2.printMatrix();
+
+            // Print solusi
+            solusi=cramer(m2, in);
+
+            // Write to file
+            ReadWrite.arrToFile(solusi, in);
         }
     }
 }
