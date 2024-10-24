94cd154b89882082b0e19b053b5983d06bb86ebf
Samantha Laqueenna Ginting <167011131+sammmine@users.noreply.github.com>
2024-10-24 12:44:22 +0700

    chore: clean up code and add input conditions

--- src/Determinant.java
+++ src/Determinant.java
@@ -7,14 +7,12 @@ public class Determinant {
         Matrix copy = m.copyMatrix(m);
 
         if (m.row != m.col) {
-            System.out.println("Determinan tidak bisa dihitung karena bukan matriks persegi.");
             return err;
         }
 
         /* Ubah Ke Matrix Eselon */
         int N=copy.row;
         for (int i = 0; i < N; i++) {
-            // Step 1: Find pivot (leading non-zero element in current column)
             if (copy.elmt(i, i) == 0) {
                 for (int j = i + 1; j < N; j++) {
                     if (copy.elmt(j, i) != 0) {
@@ -23,7 +21,6 @@ public class Determinant {
                     }
                 }
             }
-            // Step 3: Eliminate all elements below the pivot in the current column
             for (int j = i + 1; j < N; j++) {
                 if(!copy.isZeroRow(j)){
                     double factor = -copy.elmt(j, i)/copy.elmt(i,i);
@@ -41,18 +38,46 @@ public class Determinant {
 
     
 
-    public static void main(String[] args) {
-        double ans;
+    public static void call() {
+        double err = -405;
+        double solusi;
         Scanner in = new Scanner(System.in);
-        
-        Matrix mat;
-        mat=ReadWrite.txtRead(in);
 
-        System.out.println("Matriks sebelum operasi determinan:");
-        mat.printMatrix();
-        
-        ans=det(mat, in);
+        int choice;
+        choice = ReadWrite.fileOrKeys(in);
 
-        System.out.println("Determinan : "+ans);
+        if (choice == 1) {
+            System.out.print("Masukkan jumlah baris: ");
+            int rows = in.nextInt();
+            System.out.print("Masukkan jumlah kolom: ");
+            int cols = in.nextInt();
+            
+            Matrix m1 = new Matrix(rows, cols);
+            System.out.println("Masukkan matriks:");
+            m1.readMatrix(in);
+            
+            System.out.println("Matriks sebelum operasi determinan:");
+            m1.printMatrix();
+            
+            solusi=det(m1, in);
+
+        } else { // if (choice == 2)
+            Matrix m2;
+            m2=ReadWrite.txtRead(in);
+
+            System.out.println("Matriks sebelum operasi determinan:");
+            m2.printMatrix();
+
+            solusi=det(m2, in);
+        }
+
+        if (solusi == err) {
+            System.out.println("Determinan tidak bisa dihitung karena bukan matriks persegi.");
+        } else {
+            System.out.println("Determinan : "+solusi);
+        }
+
+        // Write to file
+        ReadWrite.doubleToFile(solusi, in);
     }
 }
