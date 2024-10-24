1fb7c28277270b486673cb3b2c5ecb3b256e2a61
Samantha Laqueenna Ginting <167011131+sammmine@users.noreply.github.com>
2024-10-24 14:40:49 +0700

    fix: fix bugs

--- src/RegLinier.java
+++ src/RegLinier.java
@@ -1,114 +1,104 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class RegLinier {
+
+    // Function to perform multiple linear regression
     public static double[] regLin(Matrix m, Scanner in) {
         double[] err = {-405};
-        
-        // store x values (other than first column)
-        Matrix x = new Matrix(m.row, m.col-1);
-        for (int i=0; i<m.row; i++) {
-            for (int j=1; j<m.col; j++) {
-                x.set(i, j-1, m.elmt(i, j));
+
+        // Extract the x values (columns other than the first)
+        Matrix x = new Matrix(m.row, m.col);  // Including column of ones for intercept
+        for (int i = 0; i < x.row; i++) {
+            x.set(i, 0, 1);  // First column set to 1 for intercept term
+            for (int j = 1; j < x.col; j++) {
+                x.set(i, j, m.elmt(i, j - 1));  // Shift columns by one (exclude first column from original)
             }
         }
-        
-        // store y values (first column)
-        Matrix y = new Matrix(m.row, 1);
-        for (int i=0; i<m.row; i++) {
-            y.set(i, 0, m.elmt(i, 0));
+
+        // Extract the y values (first column)
+        double[] y = new double[m.row];
+        for (int i = 0; i < m.row; i++) {
+            y[i] = m.elmt(i, 0);
         }
-        
-        // hitung x^T * x
+
+        // Compute X^T * X (normal equation)
         Matrix xt = Matrix.transposeMatrix(x);
-        Matrix xtx = Matrix.multMatrix(xt, x); // row = xt.row ; col = x.col
+        Matrix xtx = Matrix.multMatrix(xt, x);
 
-        // hitung x^T * y
-        Matrix xty = Matrix.multMatrix(xt, y); // row = xt.row ; col = y.col
+        // Compute X^T * y
+        Matrix yMatrix = new Matrix(m.row, 1);
+        for (int i = 0; i < m.row; i++) {
+            yMatrix.set(i, 0, y[i]);
+        }
+        Matrix xty = Matrix.multMatrix(xt, yMatrix);
 
-        Matrix temp = new Matrix(xt.row, x.col+y.col);
-        // gabungkan xtx dan xty
-        for (int i=0; i<temp.row; i++) {
-            for (int j=0; j<xtx.col; j++) {
-                temp.set(i, j, xtx.elmt(i, j));
-                }
-            for (int j=0; j<xty.col; i++) {
-                temp.set(i, j+xty.col, xty.elmt(i, 0));
+        // Create an augmented matrix (X^T * X | X^T * y)
+        Matrix augmentedMatrix = new Matrix(xtx.row, xtx.col + 1);
+        for (int i = 0; i < xtx.row; i++) {
+            for (int j = 0; j < xtx.col; j++) {
+                augmentedMatrix.set(i, j, xtx.elmt(i, j));
             }
-        }
-        
-        
-        Matrix solusiM = Gauss.mgauss(temp, in);
-        // Penyelesaian gauss
-        double[] solusi2 = new double[m.col-1];
-        boolean[] isFree = new boolean[m.col-1];
-        boolean unsolv=false;
-        boolean hasFreeVar = false;
-        List<Integer> freeVar = new ArrayList<>();
-        /* Lakukan Substitusi Balik */
-        for(int i=solusiM.row-1;i>=0;i--){
-            if(solusiM.isZeroRow(i)&&solusiM.elmt(i,solusiM.col-1)!=0){
-                return err;
-            }
-            else if(solusiM.isZeroRow(i)){
-                freeVar.add(i); //variabel bebas
-            }
-            double sum=0;
-            for(int j=i+1;j<solusiM.col-1;j++){
-                sum+=m.elmt(i,j)*solusi2[j];
-            }
-            solusi2[i]=(solusiM.elmt(i,solusiM.col-1)-sum)/solusiM.elmt(i,i);
-        }
-        /* Menentukan adanya variabel bebas */
-        for(int i=0;i<solusiM.row;i++){
-            boolean foundPivot = false;
-            for(int j=0;j<solusiM.col-1;j++){
-                if(solusiM.elmt(i,j)!=0){
-                    foundPivot=true;
-                    break;
-                }
-            }
-            if(!foundPivot){
-                hasFreeVar=true; //variabel bebas ditemukan
-                isFree[i]=true;
-            }
+            augmentedMatrix.set(i, xtx.col, xty.elmt(i, 0));  // Last column is X^T * y
         }
 
-        /* Keluaran */
-        if(unsolv){
+        // Solve the system using Gaussian elimination
+        double[] solution = Gauss.msolution(Gauss.mgauss(augmentedMatrix, in));
+
+        // Check if there's no solution
+        if (solution == err) {
             System.out.println("Tidak dapat mencari solusi regresi.");
             return err;
         }
-        else if(hasFreeVar){
-            System.out.println("Hasil perhitungan regresi menggunakan metode Gauss :");
-            for(int i=0;i<solusiM.col-1;i++){
-                if(isFree[i]){
-                    System.out.println("beta"+(i+1)+" : variabel bebas");
-                }
-                else{
-                    System.out.print("beta"+(i+1)+ " : ");
-                    for(int j=i+1;j<solusiM.col-1;j++){
-                        if(solusiM.elmt(i,j)!=0){
-                            System.out.print(solusiM.elmt(i,j)+" * variabel bebas ");
-                        }  
-                    }
-                    System.out.println();
-                }
-            }
-            return err;
+
+        return solution;
+    }
+
+    public static void f(double[] solusi, Scanner in, double[] taksiran) {
+        double hasil = 0;
+        System.out.println("Hasil perhitungan regresi linear berganda :");
+        String outPut = "f(x) = ";
+        String outPutH = "Dengan taksiran untuk f(";
+        
+        // Intercept
+        hasil += solusi[0];
+        outPut += Double.toString(solusi[0]);
+
+        int colIndex = 1;
+        // Linear terms (x1, x2, ...)
+        for (int i = 0; i < taksiran.length; i++) {
+            hasil += solusi[colIndex] * taksiran[i];
+            outPut += (solusi[colIndex] >= 0 ? " + " : " - ") + Math.abs(solusi[colIndex]) + "*x_" + (i+1);
+            colIndex++;
         }
-        else{
-            System.out.println("Hasil perhitungan regresi menggunakan metode Gauss :");
-            for(int i=0;i<solusi2.length;i++){
-                System.out.println("x"+(i+1)+" : "+solusi2[i]);
-            }
-            return solusi2;
+
+        // Print the result
+        outPutH += "(";
+        for (int i = 0; i < taksiran.length; i++) {
+            outPutH += (i > 0 ? ", " : "") + taksiran[i];
+        }
+        outPutH += ") adalah " + hasil;
+        
+        System.out.println(outPut);
+        System.out.println(outPutH);
+
+        // Option to write the result to a file
+        System.out.print("Tulis hasil dalam file .txt? (y/n): ");
+        String txt = in.next();
+        while (!txt.equalsIgnoreCase("y") && !txt.equalsIgnoreCase("n")) {
+            System.out.print("Input tidak valid, silahkan input kembali: ");
+            txt = in.next();
+        }
+
+        if (txt.equalsIgnoreCase("y")) {
+            StringBuilder output = new StringBuilder();
+            output.append(outPut).append("\n").append(outPutH).append("\n");
+            ReadWrite.txtWrite(in, output.toString());
         }
     }
 
     public static void call() {
         double[] ans;
+        Matrix mat;
         Scanner in = new Scanner(System.in);
 
         int choose;
@@ -120,30 +110,25 @@ public class RegLinier {
             System.out.print("Masukkan jumlah kolom: ");
             int cols = in.nextInt();
             
-            Matrix m1 = new Matrix(rows, cols);
+            mat = new Matrix(rows, cols);
             System.out.println("Masukkan matriks:");
-            m1.readMatrix(in);
-            
-            System.out.println("Matriks sebelum regresi linier:");
-            m1.printMatrix();
-
-            ans = regLin(m1, in);
+            mat.readMatrix(in);
         } else { // if (choose == 2)
-            Matrix mat;
             mat=ReadWrite.txtRead(in);
 
             System.out.println("Matriks sebelum regresi linier:");
-            mat.readMatrix(in);
+            mat.printMatrix();
+        }
 
-            ans = regLin(mat, in);
+        System.out.println("Masukkan taksiran (x1, x2, ...):");
+        double[] taksiran = new double[mat.col - 1];
+        for (int i = 0; i < taksiran.length; i++) {
+            System.out.print("x_" + (i + 1) + ": ");
+            taksiran[i] = in.nextDouble();
         }
         
-        System.out.println("Solusi regresi linier:");
-        for (int i=0; i < ans.length; i++) {
-            System.out.print("beta" + (i+1) + " = " + ans[i]);
-        }
+        ans = regLin(mat, in);
+        f(ans, in, taksiran);
 
-        // Write to file
-        ReadWrite.arrToFile(ans, in);
     }
 }
